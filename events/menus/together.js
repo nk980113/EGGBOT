d351860e2f6306be3f5d3da1c119c2ab2b2791e6
nk980113 <50331539+nk980113@users.noreply.github.com>
2021-10-31 08:05:33 +0800

    Update together.js

--- events/menus/together.js
+++ events/menus/together.js
@@ -1,7 +1,7 @@
 const { DiscordTogether } = require('discord-together');
 const together = menu => {
     if (menu.values.includes('leave')) return menu.message.edit('881');
-    if (!menu.member.voice.channel) menu.message.edit('給我加進語音頻道...唉...你得重新使用指令了...');
+    if (!menu.member.voice.channel) return menu.message.edit('給我加進語音頻道...唉...你得重新使用指令了...');
     const dcTogether = new DiscordTogether(menu.client);
     dcTogether.createTogetherCode(menu.member.voice.channel.id, menu.values[0]).then(async ({ code }) => menu.message.channel.send(code));
 };
