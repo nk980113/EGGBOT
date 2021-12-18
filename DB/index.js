2854ff1af722344fe82121b926014941342c9188
nk980113 <50331539+nk980113@users.noreply.github.com>
2021-12-18 16:07:49 +0800

    Rebuild DB & Add ESLint

--- DB/index.js
+++ DB/index.js
@@ -1,8 +1,41 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('database', 'eggeggegg', require('../token.json').dbpw, {
-    host: 'localhost',
-    dialect: 'sqlite',
-    logging: false,
-    storage: './DB/db.sqlite'
-});
-module.exports = { sequelize, DataTypes };
+const { readFileSync, writeFileSync } = require('fs');
+const copyObj = (obj, proto = null) => {
+    if (typeof obj !== 'object' || obj === null) return obj;
+    const copiedObj = Object.create(typeof proto === 'object' && proto !== null ? proto : Reflect.getPrototypeOf(obj));
+    for (const prop in obj) copiedObj[prop] = copyObj(obj[prop]);
+    return copiedObj;
+};
+const createProxyBind = (parent, child, propName) => {
+    const childCopy = copyObj(child);
+    return new Proxy(null, {
+        get(_, prop) {
+            const target = childCopy[prop];
+            if (typeof target === 'object' && target !== null) return createProxyBind(childCopy, target, prop);
+            return target;
+        },
+        set(_, prop, val) {
+            try {
+                childCopy[prop] = copyObj(val, Object.prototype);
+                parent[propName] = childCopy;
+            } catch {
+                return false;
+            }
+            return true;
+        },
+    });
+};
+const dbWrapper = filePath => {
+    const data = JSON.parse(readFileSync(filePath, { encoding: 'utf-8' }));
+    return new Proxy(null, {
+        get(_, prop) {
+            const target = data[prop];
+            if (typeof target !== 'object' || target === null) return target;
+            return createProxyBind(data, data[prop], prop);
+        },
+        set(_, prop, val) {
+            data[prop] = val;
+            writeFileSync(filePath, JSON.stringify(filePath), { encoding: 'utf-8' });
+        },
+    });
+};
+module.exports = dbWrapper;
--- refresh.js
+++ refresh.js
@@ -3,15 +3,17 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { token } = require('./token.json');
 const { clientId, guildId } = require('./config.json');
-const fs = require('fs');
-const r = new REST({version: '9'}).setToken(token);
+const { readdirSync } = require('fs');
+const { join } = require('path');
+const r = new REST({ version: '9' }).setToken(token);
 const refresh = () => {
     const cmds = [];
     const testCmds = [];
-    const cmdDirs = fs.readdirSync('./events/commands');
+    const cmdDirs = readdirSync('./events/commands');
     for (const dir of cmdDirs) {
-        const cmdFiles = fs.readdirSync(`./events/commands/${dir}`).filter(f => f.endsWith('.js'));
+        const cmdFiles = readdirSync(`./events/commands/${dir}`).filter(f => f.endsWith('.js'));
         for (const file of cmdFiles) {
+            delete require.cache[join(__dirname, 'events', 'commands', dir, file)];
             const cmd = require(`./events/commands/${dir}/${file}`);
             if (cmd.off) continue;
             if (cmd.test) {
@@ -27,22 +29,21 @@ const refresh = () => {
         try {
             await r.put(
                 Routes.applicationCommands(clientId),
-                { body: cmds }
+                { body: cmds },
             );
             logger.info('refresh:Global /commands refreshed.');
-        } catch(err) {
+        } catch (err) {
             logger.error(err);
         }
         try {
             await r.put(
                 Routes.applicationGuildCommands(clientId, guildId),
-                { body: testCmds }
+                { body: testCmds },
             );
             logger.info('refresh:Testing /commands refreshed.');
-        } catch(err) {
+        } catch (err) {
             logger.error(err);
         }
     })();
-    
-}
+};
 module.exports = refresh;
